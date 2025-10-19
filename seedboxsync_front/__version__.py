13b3a871852f5c2157a24cb6ebac6e35373c2912
Guillaume Kulakowski <guillaume@kulakowski.fr>
2025-10-19 12:08:24 +0200

    🔖 Bump v1.0.0rc1

--- seedboxsync_front/__version__.py
+++ seedboxsync_front/__version__.py
@@ -5,6 +5,6 @@
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
 #
-__version__ = "1.0.0b2"
+__version__ = "1.0.0rc1"
 __api_version__ = "1.0"
 __api_path_version__ = "v1"
